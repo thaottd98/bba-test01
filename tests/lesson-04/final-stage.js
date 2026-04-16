60a0a8b6cdf2fc55cb9fc2a6f2fa457ba7235d80
test 01 - Thao Tran <thaottd98@gmail.com>
2026-04-16 23:28:59 +0700

    final stage

---
+++ tests/lesson-04/final-stage.js
@@ -0,0 +1,14 @@
+let count = 0;
+const pairs = [];
+
+for (let a = 1; a <= 100; a++) {
+  for (let b = a + 1; b <= 100; b++) {
+    if ((a + b) % 17 === 0) {
+      pairs.push(`(${a}, ${b})`);
+      count++;
+    }
+  }
+}
+console.log("Các cặp số tìm được:");
+console.log(pairs.join(", "));
+console.log(`\nTổng cộng có: ${count} cặp số.`);
